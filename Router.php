07d81336c9a50b0b580db90655ab0f3bd7c5d66f
Hamza Sehouli <sehouli.hamza@gmail.com>
2021-11-16 20:32:39 +0100

    Added Router class which handels routing in the ap, register all the available routes, and call the requested route

---
+++ Router.php
@@ -0,0 +1,31 @@
+<?php
+
+class Router{
+    private static $getRoutes; 
+    private static $postRoutes; 
+
+    public function get($url, $fn){
+        $this->getRoutes[$url]=$fn;
+    }
+    public function post($url, $fn){
+        $this->postRoutes[$url]=$fn;
+    }
+
+    public function call(){
+        $method = $_SERVER['REQUEST_METHOD'];
+        $path = $_SERVER['PATH_INFO'] ?? '/';
+        $fn=null;
+
+        if($method === 'GET'){
+            $fn = $this->getRoutes[$path];
+        }else{
+            $fn = $this->postRoutes[$path];
+        }
+        if($fn){
+            call_user_func($fn);
+        }else{
+            echo "No route found with:$path"; 
+        }
+    }
+}
+
