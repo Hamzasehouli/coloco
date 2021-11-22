e23392f849f4a84b0bfb45d578b4580c5c3609fb
hamzasehouli <sehoulihamza@yahoo.com>
2021-11-22 11:11:50 +0100

    Finished header styling

--- views/overview.php
+++ views/overview.php
@@ -10,7 +10,37 @@ include_once '_header.php';
 ?>
 </header>
 <main>
-    <h1>overview</h1>
+    <section class="section__hero">
+        <figure class="figure__hero">
+            <h1 style="color:white;font-size:1.8rem;margin-bottom:2rem">More than 8,000 new Ads per day</h1>
+            <form class="form__hero">
+                <div class="form__control">
+                    <input placeholder="City" type="text" class="form__input">
+                </div>
+                <div class="form__control">
+                    <select type="text" class="form__select">
+                        <option selected>Flatshares</option>
+                        <option>1room flat</option>
+                        <option>Flats</option>
+                        <option>House</option>
+                    </select>
+
+                </div>
+                <div class="form__control">
+                    <select type="text" class="form__select">
+                        <option selected>Offers</option>
+                        <option>Requests</option>
+                    </select>
+
+                </div>
+                <div class="form__control">
+                    <button style="width:7rem !important" class="btn btn-primary" type="submit">Find</button>
+
+                </div>
+
+            </form>
+        </figure>
+    </section>
 </main>
 <footer>
 
