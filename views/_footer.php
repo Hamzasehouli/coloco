7be8215e3973f17024365c84d8a6ee3f04ebd9f1
hamzasehouli <sehoulihamza@yahoo.com>
2022-01-10 09:20:22 +0100

    Finished footer styling

--- views/_footer.php
+++ views/_footer.php
@@ -4,18 +4,19 @@
             <form class="newsletter">
 
 
-
+                <!--
                 <div class="col-auto">
                     <p class="pt-2">
                         <strong>Sign up for our newsletter</strong>
                     </p>
-                </div>
+                </div> -->
 
                 <div class="col-md-5 col-12">
 
-                    <div class="form-outline form-white mb-4">
-                        <input type="email" id="form5Example21" class="form-control" />
-                        <label class="form-label" for="form5Example21">Email address</label>
+                    <div style="margin-right:1rem">
+                        <input style="width:40rem;" placeholder="Newsletter" type="email" id="form5Example21"
+                            class="form-control" />
+                        <!-- <label class="form-label" for="form5Example21">Email address</label> -->
                     </div>
                 </div>
                 <div class="col-auto">
